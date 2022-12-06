0f29074bcd61c1f7eb280ae0a9ef414826c88f78
MINYUKYUNG <minew0223@gmail.com>
2022-12-06 11:30:16 +0900

    Feature: Preview, Submit 페이지 응답 리스트 컴포넌트 생성

---
+++ src/components/UseQuestion/Response.tsx
@@ -0,0 +1,162 @@
+import { useDispatch } from 'react-redux';
+import { quesType } from '../../utils';
+import { quesConst } from '../../constants';
+import { ChangeEvent, useState, useRef } from 'react';
+import { MdKeyboardArrowDown, MdOutlineErrorOutline } from 'react-icons/md';
+import { textResp, oneOptionResp, multipleOptionResp } from '../../store/questions';
+
+function Response(item: quesType.QuestionsGuard) {
+  const dispatch = useDispatch();
+  const [hidden, setHidden] = useState(true);
+  const trueOption = useRef(0);
+  const checkOption = useRef(false);
+
+  const openClose = (bool: boolean) => setHidden(bool);
+
+  const textRespBtn = (e: ChangeEvent<HTMLInputElement>) => { // 단답형, 장문형 응답
+    const value = e.target.value;
+    dispatch(textResp({ sendId: item.id, value }));
+  };
+
+  const oneOptionRespBtn = (optionIndex: number) => { // 객관식 질문, 드롭박스 응답
+    if (hidden === false && item.type === quesConst.DROPDOWN_TYPE) {
+      openClose(true);
+      trueOption.current = optionIndex;
+    };
+
+    dispatch(oneOptionResp({ sendId: item.id, optionIndex }));
+  };
+
+  const multipleOptionRespBtn = (e: ChangeEvent<HTMLInputElement>, optionIndex: number) => { // 체크박스 응답
+    const checked = e.target.checked;
+    dispatch(multipleOptionResp({ sendId: item.id, optionIndex, checked }));
+  };
+
+  const meetRequirements = () => { // 필수 항목 응답 유무 확인 (미응답시 submit페이지로 이동불가)
+    return (
+      <div className="flex justify-end text-red-600 pt-6">
+        <MdOutlineErrorOutline size="1.5em" />
+        <span className="inline-block text-sm pl-2 leading-6">필수 질문입니다.</span>
+      </div>
+    );
+  };
+
+  if (quesConst.SHORT_TYPE === item.type) {
+    return (
+      <div>
+        <input 
+          type="text" 
+          placeholder="내 답변" 
+          className="focus-visible:outline-none border-b-2 
+          pt-6 pb-1 w-1/2 focus:border-rose-400" 
+          onChange={textRespBtn} 
+          value={item.textResponse || ''} 
+        />
+        {item.required.condition && item.required.meet === false ? meetRequirements(): null}
+      </div>
+    );
+  } else if (quesConst.ESSAY_TYPE === item.type) {
+    return (
+      <div>
+        <input 
+          type="text" 
+          placeholder="내 답변" 
+          className="focus-visible:outline-none border-b-2  
+          pt-6 pb-1 w-full focus:border-rose-400" 
+          onChange={textRespBtn} 
+          value={item.textResponse || ''} 
+        />
+        {item.required.condition && item.required.meet === false ? meetRequirements(): null}
+      </div>
+    );
+  } else if (quesConst.DROPDOWN_TYPE === item.type) {
+    const dropdown = item.options.map((option, index) => {
+      if (option.optionResponse) {
+        trueOption.current = index;
+        checkOption.current = true;
+      };
+
+      return (
+        <li onClick={() => oneOptionRespBtn(index)} key={index}>
+          <button className="active:bg-rose-400 hover:bg-rose-400 hover:text-white">
+            {option.text}
+          </button>
+        </li>
+      );
+    });
+
+    const select = () => {
+      const checkReset = !(item.options[trueOption.current].optionResponse);
+      if (checkOption.current && checkReset) {
+        trueOption.current = 0;
+        checkOption.current = false;
+        return '선택해주세요';
+      } else if (checkOption.current) return item.options[trueOption.current].text;
+      else return '선택해주세요';
+    };
+
+    return (
+      <div className="pt-6">
+        <div className="dropdown dropdown-end w-1/3">
+          <label 
+            tabIndex={0} 
+            className="btn btn-outline w-full border-gray-300 
+            hover:bg-transparent hover:text-black hover:border-gray-300 
+            flex justify-between"
+            onClick={() => openClose(false)}
+          >
+            {select()}
+            <MdKeyboardArrowDown size="1.5em" />
+          </label>
+          <ul 
+            tabIndex={0} 
+            className={`dropdown-content menu p-2 shadow bg-base-100 rounded-box w-full 
+            ${hidden ? 'hidden': ''}`}
+          >
+            {dropdown}
+          </ul>
+        </div>
+        {item.required.condition && item.required.meet === false ? meetRequirements(): null}
+      </div>
+    );
+  };
+
+  const showOptions = item.options.map((option, index) => {
+    const choice = () => (
+      <input 
+        type="radio" 
+        name={`radio-${item.id}`} 
+        className="radio mr-4" 
+        onChange={() => oneOptionRespBtn(index)} 
+        checked={option.optionResponse || false}
+      />
+    );
+    const checkbox = () => (
+      <input 
+        type="checkbox" 
+        className="checkbox mr-4" 
+        onChange={(e) => multipleOptionRespBtn(e, index)} 
+        checked={option.optionResponse || false}
+      />
+    );
+
+    return (
+      <div className="form-control pt-6" key={index}>
+        <label className="label cursor-pointer justify-start px-0">
+          {item.type === quesConst.CHOICE_TYPE ? choice(): checkbox()}
+          <span className="label-text">{option.text}</span> 
+        </label>
+      </div>
+    );
+  });
+
+
+  return (
+    <div>
+      {showOptions}
+      {item.required.condition && item.required.meet === false ? meetRequirements(): null}
+    </div>
+  )
+};
+
+export default Response;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CreateQuestion } from './CreateQuestion';
+export { default as CreateTitle } from './CreateTitle';
+export { default as UseQuestion } from './UseQuestion';
+export { default as UseTitle } from './UseTitle';
