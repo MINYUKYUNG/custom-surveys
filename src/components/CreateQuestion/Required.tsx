fdd6e59584db19342604405ee6ed24ea0ac295c2
MINYUKYUNG <minew0223@gmail.com>
2022-12-06 11:28:33 +0900

    Feature: 질문 필수 항목 설정 유무

---
+++ src/components/CreateQuestion/Required.tsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux';
+import { quesType } from '../../utils';
+import { editRequired } from '../../store/questions';
+
+function Required(item: quesType.QuestionsGuard) {
+  const dispatch = useDispatch();
+
+  const editRequiredBtn = () => { // 필수 항목 설정 유무
+    const sendId = item.id;
+    dispatch(editRequired(sendId));
+  };
+
+  return (
+    <div className="pl-4 border-l">
+      <label className="label cursor-pointer">
+        <span className="label-text pr-2">필수</span> 
+        <input type="checkbox" 
+          className="toggle toggle-sm checked:bg-rose-400 checked:border-rose-400" 
+          onChange={editRequiredBtn} 
+          checked={item.required.condition}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default Required;
